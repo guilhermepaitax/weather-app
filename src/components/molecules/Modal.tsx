f8adae0a181c017d9708e42a65fbcc5cc3b43fb5
guilhermepaitax <guilhermepaitax16@gmail.com>
2022-01-29 18:09:23 -0300

    fix: change modal props

--- src/components/molecules/Modal.tsx
+++ src/components/molecules/Modal.tsx
@@ -2,13 +2,12 @@ import { useCallback } from 'react'
 import {
   Modal as ModalChakra,
   ModalOverlay,
-  ModalContent,
-  ModalProps
+  ModalContent
 } from '@chakra-ui/react'
 import { useAppSelector, useAppDispatch } from '../../hooks/redux'
 import { closeModal } from '../../store/modal/modalSlice'
 
-export const Modal: React.FC<ModalProps> = () => {
+export const Modal = () => {
   const dispatch = useAppDispatch()
   const { modalProps, children, isOpen } = useAppSelector(state => state.modal)
 
